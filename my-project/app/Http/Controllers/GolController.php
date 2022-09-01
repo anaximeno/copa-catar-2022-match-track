ed0924f1fc838aac745c13e4e190179374d3f4e3
anaximeno <anaximenobrito@gmail.com>
2022-09-01 15:09:46 -0100

    feat: modelo e controlador para gols

---
+++ my-project/app/Http/Controllers/GolController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Gol;
+use Illuminate\Http\Request;
+
+class GolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Gol  $gol
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Gol $gol)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Gol  $gol
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Gol $gol)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Gol  $gol
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Gol $gol)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Gol  $gol
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Gol $gol)
+    {
+        //
+    }
+}
