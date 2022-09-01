66486e5c28610e0056381179375e65658b9f4af5
anaximeno <anaximenobrito@gmail.com>
2022-09-01 19:03:22 -0100

    feat: adicionados métodos para o gol

--- my-project/app/Models/Gol.php
+++ my-project/app/Models/Gol.php
@@ -8,4 +8,22 @@ use Illuminate\Database\Eloquent\Model;
 class Gol extends Model
 {
     use HasFactory;
+
+    // O jogador que marcou o golo
+    function jogador()
+    {
+        return $this->hasOne(\App\Models\Jogador::class, 'id', 'id_jogador');
+    }
+
+    // A equipa que marcou o golo
+    function equipa()
+    {
+        return $this->hasOne(\App\Models\Confronto::class, 'id', 'id_confronto');
+    }
+
+    // O confronto decorrente no momento
+    function confronto()
+    {
+        return $this->hasOne(\App\Models\Confronto::class, 'id', 'id_confronto');
+    }
 }
