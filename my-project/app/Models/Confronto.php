4296c28d74fb4d2efed8f526556236044c457847
anaximeno <anaximenobrito@gmail.com>
2022-09-01 15:55:51 -0100

    feat: novo método para modelo Confronto

--- my-project/app/Models/Confronto.php
+++ my-project/app/Models/Confronto.php
@@ -10,4 +10,27 @@ class Confronto extends Model
     use HasFactory;
 
     protected $table = 'confrontos';
+
+    // Retorna o árbitro principal do jogo.
+    function arbitro()
+    {
+        return $this->hasOne(\App\Models\Arbitro::class, 'id', 'id_arbitro_principal');
+    }
+
+    // Retorna a equipe de casa
+    function equipaCasa()
+    {
+        return $this->hasOne(\App\Models\Equipa::class, 'id', 'id_equipa_casa');
+    }
+
+    // Retorna a equipe de visita
+    function equipaVisita()
+    {
+        return $this->hasOne(\App\Models\Equipa::class, 'id', 'id_equipa_visita');
+    }
+
+    function jogadoresEmCampo()
+    {
+        return $this->hasMany(\App\Models\JogadorEmCampo::class, 'id', 'id_confronto');
+    }
 }
