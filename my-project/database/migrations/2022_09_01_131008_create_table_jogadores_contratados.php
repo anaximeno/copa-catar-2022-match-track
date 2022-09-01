77b471e3853db43f906c1b29611dd5d26754b91b
anaximeno <anaximenobrito@gmail.com>
2022-09-01 12:22:53 -0100

    fix: corrige nome da tabela de referencia para equipes

---
+++ my-project/database/migrations/2022_09_01_131008_create_table_jogadores_contratados.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jogadores_contratados', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_equipa');
+            $table->unsignedBigInteger('id_jogador');
+            $table->foreign('id_equipa')
+                  ->references('id')
+                  ->on('equipes');
+            $table->foreign('id_jogador')
+                  ->references('id')
+                  ->on('jogadores');
+            $table->primary(['id_equipa', 'id_jogador']);
+            $table->dateTime('inicio_do_contrato');
+            $table->dateTime('expiracao_do_contrato');
+            $table->tinyInteger('numero_da_camisa');
+            $table->string('posicao');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jogadores_contratados');
+    }
+};
