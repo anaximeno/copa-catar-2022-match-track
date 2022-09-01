0a19ca3fdd13d05545a51dc23f8bc6c9c3b7fcf1
anaximeno <anaximenobrito@gmail.com>
2022-09-01 15:08:59 -0100

    feat: modelo e controlador para jogador em campo

---
+++ my-project/app/Http/Controllers/JogadorEmCampoController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\JogadorEmCampo;
+use Illuminate\Http\Request;
+
+class JogadorEmCampoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\JogadorEmCampo  $jogadorEmCampo
+     * @return \Illuminate\Http\Response
+     */
+    public function show(JogadorEmCampo $jogadorEmCampo)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\JogadorEmCampo  $jogadorEmCampo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(JogadorEmCampo $jogadorEmCampo)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\JogadorEmCampo  $jogadorEmCampo
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, JogadorEmCampo $jogadorEmCampo)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\JogadorEmCampo  $jogadorEmCampo
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(JogadorEmCampo $jogadorEmCampo)
+    {
+        //
+    }
+}
