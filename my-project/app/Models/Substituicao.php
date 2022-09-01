60640298143c95a44090aa0e469b65b0e6f0ad1c
anaximeno <anaximenobrito@gmail.com>
2022-09-01 18:58:39 -0100

    feat: adicionados métodos para a substituição

--- my-project/app/Models/Substituicao.php
+++ my-project/app/Models/Substituicao.php
@@ -10,4 +10,28 @@ class Substituicao extends Model
     use HasFactory;
 
     protected $table = 'substituicoes';
+
+    // O jogador que saiu
+    function jogadorQueSaiu()
+    {
+        return $this->hasOne(\App\Models\Jogador::class, 'id', 'id_jogador_sai');
+    }
+
+    // O jogador que entrou
+    function jogadorQueEntrou()
+    {
+        return $this->hasOne(\App\Models\Jogador::class, 'id', 'id_jogador_entra');
+    }
+
+    // A equipa em que a substituição foi feita
+    function equipa()
+    {
+        return $this->hasOne(\App\Models\Equipa::class, 'id', 'id_equipa');
+    }
+
+    // O confronto em decorrimento no momento da substituição
+    function confronto()
+    {
+        return $this->hasOne(\App\Models\Confronto::class, 'id', 'id_confronto');
+    }
 }
