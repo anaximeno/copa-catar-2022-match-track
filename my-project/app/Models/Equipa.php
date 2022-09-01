152e23e660cadb3b7a1787dac8ad020c982ec556
anaximeno <anaximenobrito@gmail.com>
2022-09-01 15:41:24 -0100

    feat: novos métodos adicionados

--- my-project/app/Models/Equipa.php
+++ my-project/app/Models/Equipa.php
@@ -10,4 +10,27 @@ class Equipa extends Model
     use HasFactory;
 
     protected $table = 'equipes';
+
+    // Retorna os contratos com os jogadores.
+    function jogadoresContratados()
+    {
+        return $this->hasMany(\App\Models\JogadorContratado::class, 'id_equipa');
+    }
+
+    // Retorna os confrontos em casa da equipa.
+    function confrontosEmCasa()
+    {
+        return $this->hasMany(\App\Models\Confronto::class, 'id_equipa_casa');
+    }
+
+    // Retorna os confrontos em visita da equipa.
+    function confrontosEmVisita()
+    {
+        return $this->hasMany(\App\Models\Confronto::class, 'id_equipa_visita');
+    }
+
+    // Retorna todos os confrontos da equipa
+    // function confrontos() // TODO
+    // {
+    // }
 }
