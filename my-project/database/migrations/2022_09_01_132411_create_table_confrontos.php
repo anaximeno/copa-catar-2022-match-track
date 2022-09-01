9d604c094266b79e356eee5bb3f286645250c340
anaximeno <anaximenobrito@gmail.com>
2022-09-01 12:37:22 -0100

    feat: migração para criar a tabela confrontos

---
+++ my-project/database/migrations/2022_09_01_132411_create_table_confrontos.php
@@ -0,0 +1,55 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('confrontos', function (Blueprint $table) {
+            $table->id();
+            $table->string('local');
+            $table->dateTime('inicio');
+            $table->dateTime('fim')->nullable();
+            $table->date('dia');
+            $table->string('estadio');
+            $table->unsignedBigInteger('id_equipa_casa');
+            $table->unsignedBigInteger('id_equipa_visita');
+            $table->unsignedBigInteger('id_arbitro_principal');
+
+            $table->boolean('terminou');
+            $table->string('rodada')->nullable();
+
+            $table->foreign('id_equipa_casa')
+                  ->references('id')
+                  ->on('equipes');
+            $table->foreign('id_equipa_visita')
+                  ->references('id')
+                  ->on('equipes');
+            $table->foreign('id_arbitro_principal')
+                  ->references('id')
+                  ->on('arbitros');
+
+            $table->timestamps();
+        });
+
+        // TODO: check id_equipa_casa != id_equipa_visita
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('confrontos');
+    }
+};
