beffda02255f00cb36b6de2cee59552ac08b5787
anaximeno <anaximenobrito@gmail.com>
2022-09-01 19:02:18 -0100

    feat: adicionados métodos para o cartão

--- my-project/app/Models/Cartao.php
+++ my-project/app/Models/Cartao.php
@@ -10,4 +10,22 @@ class Cartao extends Model
     use HasFactory;
 
     protected $table = 'cartoes';
+
+    // O jogador que recebeu o cartão
+    function jogador()
+    {
+        return $this->hasOne(\App\Models\Jogador::class, 'id', 'id_jogador');
+    }
+
+    // A equipa que recebeu cartão
+    function equipa()
+    {
+        return $this->hasOne(\App\Models\Confronto::class, 'id', 'id_confronto');
+    }
+
+    // O confronto decorrente no momento
+    function confronto()
+    {
+        return $this->hasOne(\App\Models\Confronto::class, 'id', 'id_confronto');
+    }
 }
