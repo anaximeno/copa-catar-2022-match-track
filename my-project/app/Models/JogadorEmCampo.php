5b4cbd192669608016776b4fa685d2472c6bf3da
anaximeno <anaximenobrito@gmail.com>
2022-09-01 16:14:38 -0100

    feat: novo método para modelo JogadorEmCampo

--- my-project/app/Models/JogadorEmCampo.php
+++ my-project/app/Models/JogadorEmCampo.php
@@ -10,4 +10,16 @@ class JogadorEmCampo extends Model
     use HasFactory;
 
     protected $table = 'jogador_em_campo';
+
+    // Retorna o jogador em campo. Referencia o modelo JogadorContratado
+    function jogadorDeEquipa()
+    {
+        return $this->hasOne(\App\Models\JogadorContratado::class, 'id', 'id_jogador');
+    }
+
+    // Retorna o confronto em que o jogador está participando
+    function confronto()
+    {
+        return $this->hasOne(\App\Models\Confronto::class, 'id', 'id_confronto');
+    }
 }
