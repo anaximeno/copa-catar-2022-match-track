4c151c42eaf52bc3e3145a28e2b38ef9763f7789
anaximeno <anaximenobrito@gmail.com>
2022-09-06 01:26:23 -0100

    feat: removed debug banner

--- flutter_frontend/lib/app.dart
+++ flutter_frontend/lib/app.dart
@@ -8,6 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Copa Catar 2022 - Match Tracker',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.purple,
       ),
