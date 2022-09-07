28f60209075cca96b720212a8487d49af1131100
anaximeno <anaximenobrito@gmail.com>
2022-09-07 17:08:43 -0100

    fix: fixed extension of the asset

--- flutter_frontend/lib/misc/assets.dart
+++ flutter_frontend/lib/misc/assets.dart
@@ -1,5 +1,5 @@
 /// This file contains list of constants, relating to the assets of this program.
 
 const imgJogadorDeFutebol = 'images/jogador_de_futebol.jpeg';
-const imgClubeDeFutebol = 'images/clube_de_futebol.jpeg';
+const imgClubeDeFutebol = 'images/clube_de_futebol.png';
 const imgEstadioDeFutebol = 'images/stadium.jpg';
