a3c366ab2161608a70a00a87154c41ba9496351f
anaximeno <anaximenobrito@gmail.com>
2022-09-07 16:50:07 -0100

    feat: add new dir misc

---
+++ flutter_frontend/lib/misc/assets.dart
@@ -0,0 +1,5 @@
+/// This file contains list of constants, relating to the assets of this program.
+
+const imgJogadorDeFutebol = 'images/jogador_de_futebol.jpeg';
+const imgClubeDeFutebol = 'images/clube_de_futebol.jpeg';
+const imgEstadioDeFutebol = 'images/stadium.jpg';
