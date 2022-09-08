34156d1d5674e910716c3499822c25bba7d0388b
anaximeno <anaximenobrito@gmail.com>
2022-09-08 21:26:55 -0100

    docs: update info on configs

--- flutter_frontend/lib/misc/configs.dart
+++ flutter_frontend/lib/misc/configs.dart
@@ -5,3 +5,9 @@ const laravelPort = '8000';
 
 /// Indereço onde o servidor laravel deve estar executando
 const ipAddress = 'http://$hostIpAddress:$laravelPort';
+
+/** NOTE:
+ * O IP tem de ser definido numa constante pois caso se queira testar a aplicação no celular,
+ * isso vai garantir acesso do celular ao servidor Laravel se o computador e o celular
+ * estiverem conectados na mesma rede Wifi.
+ */
