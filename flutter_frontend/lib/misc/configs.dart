fab57a66d584e24d0f74d44722c87240eeac87e9
anaximeno <anaximenobrito@gmail.com>
2022-09-08 20:48:50 -0100

    refactor: update ip address handling

--- flutter_frontend/lib/misc/configs.dart
+++ flutter_frontend/lib/misc/configs.dart
@@ -1,1 +1,7 @@
-const ipAddress = 'http://192.168.1.69:8000';
+/// Esta constante contém o indereço IP da sua máquina, por favor trocar caso
+/// estiver em uma máquina diferente.
+const hostIpAddress = '192.168.1.69';
+const laravelPort = '8000';
+
+/// Indereço onde o servidor laravel deve estar executando
+const ipAddress = 'http://$hostIpAddress:$laravelPort';
